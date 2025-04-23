dd6e40146876668481d5b5ad5021cf65368e1691
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-23 11:13:07 +0530

    Multi Threading using Task

---
+++ AsyncProgramming/MultiThreading.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    class MultiThreading
+    {
+        //static void Main(string[] args)
+        //{
+        //    // Create a new thread
+        //    Thread thread = new Thread(new ThreadStart(ThreadMethod));
+        //    thread.Start();
+        //    // Main thread continues to run
+        //    Console.WriteLine("Main thread is running...");
+        //    // Wait for the thread to finish
+        //    thread.Join();
+        //    Console.WriteLine("Thread has finished.");
+        //}
+        //static void ThreadMethod()
+        //{
+        //    Console.WriteLine("Thread is running...");
+        //    Thread.Sleep(2000); // Simulate work
+        //    Console.WriteLine("Thread work is done.");
+        //}
+
+        static void Main()
+        {
+            Task task = Task.Run(() => {
+                Task.Delay(15000).Wait(); // simulate work
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Task thread: {i}");
+                    Task.Delay(500); // simulate work
+                }
+            });
+
+            Console.WriteLine("Main thread continues...");
+
+            task.Wait(); // Wait for the task to finish
+            Console.WriteLine("Task completed.");
+            Console.ReadKey();
+        }
+    }
+}
